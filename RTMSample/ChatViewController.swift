d39d1806ee92ac3f740e5a549a98078d532cc599
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2021-08-24 20:00:15 +0900

    save the code

--- RTMSample/ChatViewController.swift
+++ RTMSample/ChatViewController.swift
@@ -39,5 +39,34 @@ class ChatViewController: UIViewController, ShowAlertProtocol {
 //MARK: - AgoraRtmDelegate
 
 extension ChatViewController: AgoraRtmDelegate {
+    func rtmKit(_ kit: AgoraRtmKit, connectionStateChanged state: AgoraRtmConnectionState, reason: AgoraRtmConnectionChangeReason) {
+        showAlert("connection state changed: \(state.rawValue)") { [weak self] (_) in
+            if reason == .remoteLogin, let strongSelf = self {
+                strongSelf.navigationController?.popToRootViewController(animated: true)
+            }
+        }
+    }
+}
+
+//MARK: - AgoraRtmChannelDelegate
+
+extension ChatViewController: AgoraRtmChannelDelegate {
+    //チャンネルに誰かが参加した時に発生する
+    func channel(_ channel: AgoraRtmChannel, memberJoined member: AgoraRtmMember) {
+        DispatchQueue.main.async { [unowned self] in
+            self.showAlert("DEBUG: \(member.userId) join")
+        }
+    }
     
+    //誰かがチャンネルから退室した時に発生する
+    func channel(_ channel: AgoraRtmChannel, memberLeft member: AgoraRtmMember) {
+        DispatchQueue.main.async { [unowned self] in
+            self.showAlert("DEBUG: \(member.userId) left")
+        }
+    }
+    
+    //チャンネル内でメッセージを受け取ったときに発生する
+    func channel(_ channel: AgoraRtmChannel, messageReceived message: AgoraRtmMessage, from member: AgoraRtmMember) {
+        <#code#>
+    }
 }
